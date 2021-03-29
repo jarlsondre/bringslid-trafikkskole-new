99fa1f237594e5e74c32e3fdae9d060e6d9a1e51
Jarl Sondre Sæther <jssaethe@stud.ntnu.no>
2021-03-29 13:09:54 +0200

    updated prices, changed description

--- src/components/Home/News.jsx
+++ src/components/Home/News.jsx
@@ -9,7 +9,7 @@ class News extends Component {
       {
         title: "Velkommen til Bringslid Trafikkskole",
         text:
-          "Hei, og velkommen til Bringslid Trafikkskole. Vi er en trafkkskole som holder til i HTL-bygget i Elnesvågen. Kjøreskolen drives av Yvonne Bringslid som har vært kjørelærer i over 20 år. Vi tilbyr konkurransedyktige priser og opplæring av høyeste kvalitet. For å bestille time, kontakt Yvonne på 91 51 40 04 eller på yvonne@bringslidtrafikkskole.no",
+          "Hei, og velkommen til Bringslid Trafikkskole. Vi er en trafkkskole som holder til i HTL-bygget i Elnesvågen. Kjøreskolen drives av Yvonne Bringslid som har vært kjørelærer i over 30 år. Vi tilbyr konkurransedyktige priser og opplæring av høyeste kvalitet. For å bestille time, kontakt Yvonne på 91 51 40 04 eller på yvonne@bringslidtrafikkskole.no",
         id: 3,
         storyImage: TeslaImage,
       },
